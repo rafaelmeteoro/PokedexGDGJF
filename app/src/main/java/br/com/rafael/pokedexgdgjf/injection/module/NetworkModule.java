62df971806d477988d0bcc82edfe773e4016bf32
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2016-08-26 11:20:42 -0300

    Adicionando Module Network

---
+++ app/src/main/java/br/com/rafael/pokedexgdgjf/injection/module/NetworkModule.java
@@ -0,0 +1,72 @@
+package br.com.rafael.pokedexgdgjf.injection.module;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
+import br.com.rafael.pokedexgdgjf.BuildConfig;
+import br.com.rafael.pokedexgdgjf.PokedexApplication;
+import dagger.Module;
+import dagger.Provides;
+import okhttp3.Cache;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by rafael on 8/26/16.
+ **/
+@Module
+public class NetworkModule {
+
+    private String baseUrl;
+
+    public NetworkModule(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    @Provides
+    @Singleton
+    Cache provideOkHttpCache(PokedexApplication application) {
+        int cacheSize = 10 * 1024 * 1024;
+        return new Cache(application.getCacheDir(), cacheSize);
+    }
+
+    @Provides
+    @Singleton
+    Gson provideGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        return gsonBuilder.create();
+    }
+
+    @Provides
+    @Singleton
+    HttpLoggingInterceptor provideLogginInterceptor() {
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+        loggingInterceptor.setLevel(BuildConfig.DEBUG ? HttpLoggingInterceptor.Level.BODY : HttpLoggingInterceptor.Level.NONE);
+        return loggingInterceptor;
+    }
+
+    @Provides
+    @Singleton
+    OkHttpClient provideOkHttpClient(Cache cache, HttpLoggingInterceptor loggin) {
+        OkHttpClient.Builder client = new OkHttpClient.Builder();
+        client.cache(cache);
+        client.addInterceptor(loggin);
+        return client.build();
+    }
+
+    @Provides
+    @Singleton
+    Retrofit provideRetrofit(Gson gson, OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .client(okHttpClient)
+                .build();
+    }
+}
