42b2871160433a1c9b62221ccda7bb5bd1feffd2
Rafael Alves Feliciano <fael.anjelus@gmail.com>
2016-09-08 13:10:51 -0300

    Teste de instrumentação

---
+++ app/src/androidTest/java/br/com/rafael/pokedexgdgjf/PokedexActivityTest.java
@@ -0,0 +1,55 @@
+package br.com.rafael.pokedexgdgjf;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.RuleChain;
+import org.junit.rules.TestRule;
+import org.junit.runner.RunWith;
+
+import br.com.rafael.pokedexgdgjf.data.model.Pokedex;
+import br.com.rafael.pokedexgdgjf.test.common.TestComponentRule;
+import br.com.rafael.pokedexgdgjf.ui.pokedex.PokedexActivity;
+import rx.Observable;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.not;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by rafael on 9/4/16.
+ **/
+@RunWith(AndroidJUnit4.class)
+public class PokedexActivityTest {
+
+    public final TestComponentRule component =
+            new TestComponentRule(InstrumentationRegistry.getTargetContext());
+    public final ActivityTestRule<PokedexActivity> main =
+            new ActivityTestRule<>(PokedexActivity.class, false, false);
+
+    // TestComponentRule needs to go first to make sure the Dagger ApplicationTestComponent is set
+    // in the Application before any Activity is launched.
+    @Rule
+    public TestRule chain = RuleChain.outerRule(component).around(main);
+
+    @Test
+    public void errorViewsDisplayWhenLoadingContentFails() throws InterruptedException {
+        when(component.getMockDataManager().getPokedex())
+                .thenReturn(Observable.<Pokedex>error(new RuntimeException()));
+        main.launchActivity(null);
+
+        onView(withId(R.id.loading_view))
+                .check(matches(isDisplayed()));
+        onView(withId(R.id.error_view))
+                .check(matches(isDisplayed()));
+    }
+}
